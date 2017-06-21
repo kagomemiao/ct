c97d56e2201d163907ee5b7f85c11cd449c2e84a
kagomemiao <yusie@live.cn>
2017-06-21 13:21:38 +0800

    html sorter ignore case

--- public/javascripts/htmlsorter.js
+++ public/javascripts/htmlsorter.js
@@ -1,6 +1,6 @@
 function htmlSorter(a, b) {
-	var a = $(a).text();
-  var b = $(b).text();
+	var a = $(a).text().toLowerCase();
+  var b = $(b).text().toLowerCase();
   if (a < b) return -1;
   if (a > b) return 1;
   return 0;
